472a0659d1234eb3342182efc1fa41b873e68434
n0rrman <email@henriknorrman.com>
2023-12-09 23:52:36 +0700

    four-card-feature-section completed

--- four-card-feature-section/src/app/page.tsx
+++ four-card-feature-section/src/app/page.tsx
@@ -1,22 +1,79 @@
+import Image from "next/image";
+
+import calcIcon from "public/icon-calculator.svg";
+import karmaIcon from "public/icon-karma.svg";
+import superIcon from "public/icon-supervisor.svg";
+import teamIcon from "public/icon-team-builder.svg";
+
 export default function Home() {
   return (
-    <div>
-      <h2 className="">Reliable, efficient delivery</h2>
-      <h1>Powered by Technology</h1>
-      <p>
-        Our ArtificialIntelligence powered tools use millions of project data
-        points to ensure that your project is successful
-      </p>
-      <h2>Supervisor</h2>
-      <p>Monitors activity to identify project roadblocks</p>
-      <h2>Team Builder</h2>
-      <p>
-        Scans our talent network to create the optimal team for your project
-      </p>
-      <h2>Karma</h2>
-      <p>Regularly evaluates our talent to ensure quality</p>
-      <h2>Calculator</h2>
-      <p>Uses data from past projects to provide better delivery estimates</p>
+    <div className="flex items-center justify-center bg-veryLightGray min-h-screen text-veryDarkBlue">
+      <main className="flex flex-col items-center justify-center">
+        <h2 className="font-extralight text-[2.265rem] pt-8">
+          Reliable, efficient delivery
+        </h2>
+        <h1 className="font-semibold text-[2.265rem] -mt-1">
+          Powered by Technology
+        </h1>
+        <p className="font-extralight text-[0.975rem] text-center w-[32rem] py-4">
+          Our Artificial Intelligence powered tools use millions of project data
+          points to ensure that your project is successful
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:grid-rows-2 pt-12 pb-8 p-5 gap-[1.85rem]">
+          <div className="rounded-lg overflow-hidden shadow-xl bg-white w-[21.875rem] h-[15.625rem] xl:row-span-2 xl:my-auto">
+            <div className="px-8 py-7 border-t-4 border-primaryCyan">
+              <h2 className="font-semibold text-[1.225rem] pb-2">Supervisor</h2>
+              <p className="font-extralight text-sm">
+                Monitors activity to identify project roadblocks
+              </p>
+            </div>
+            <div className="flex justify-end pb-8 pr-8">
+              <Image src={superIcon} alt="" />
+            </div>
+          </div>
+
+          {/* <div className="flex flex-col space-y-6"> */}
+          <div className="rounded-lg overflow-hidden shadow-xl bg-white w-[21.875rem] h-[15.625rem]">
+            <div className="p-8 border-t-4 border-primaryRed">
+              <h2 className="font-semibold text-lg pb-2">Team Builder</h2>
+              <p className="font-extralight text-sm">
+                Scans our talent network to create the optimal team for your
+                project
+              </p>
+            </div>
+            <div className="flex justify-end pb-8 pr-8">
+              <Image src={teamIcon} alt="" />
+            </div>
+          </div>
+
+          <div className="rounded-lg overflow-hidden shadow-xl bg-white w-[21.875rem] h-[15.625rem] xl:order-last">
+            <div className="p-8 border-t-4 border-primaryOrange">
+              <h2 className="font-semibol text-lg pb-2d">Karma</h2>
+              <p className="font-extralight text-sm">
+                Regularly evaluates our talent to ensure quality
+              </p>
+            </div>
+            <div className="flex justify-end pb-8 pr-8">
+              <Image src={karmaIcon} alt="" />
+            </div>
+          </div>
+          {/* </div> */}
+
+          <div className="rounded-lg overflow-hidden shadow-xl bg-white w-[21.875rem] h-[15.625rem] xl:my-auto xl:col-3 xl:row-span-2">
+            <div className="p-8 border-t-4 border-primaryBlue ">
+              <h2 className="font-semibold text-lg pb-2">Calculator</h2>
+              <p className="font-extralight text-sm">
+                Uses data from past projects to provide better delivery
+                estimates
+              </p>
+            </div>
+            <div className="flex justify-end pb-8 pr-8">
+              <Image src={calcIcon} alt="" />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
--- four-card-feature-section/tailwind.config.ts
+++ four-card-feature-section/tailwind.config.ts
@@ -6,7 +6,20 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primaryRed: 'hsl(var(--color-primaryRed) / <alpha-value>)',
+        primaryCyan: 'hsl(var(--color-primaryCyan) / <alpha-value>)',
+        primaryOrange: 'hsl(var(--color-primaryOrange) / <alpha-value>)',
+        primaryBlue: 'hsl(var(--color-primaryBlue) / <alpha-value>)',
+        
+        grayishBlue: 'hsl(var(--color-grayishBlue) / <alpha-value>)',
+        veryDarkBlue: 'hsl(var(--color-veryDarkBlue) / <alpha-value>)',
+        veryLightGray: 'hsl(var(--color-veryLightGray) / <alpha-value>)'
+      },
+    },
+  },
   plugins: [],
 }
 export default config
