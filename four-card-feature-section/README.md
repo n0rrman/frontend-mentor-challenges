e7e5c478b1cb4527250a34d1f8a127dee2245c60
n0rrman <email@henriknorrman.com>
2023-12-11 19:26:50 +0700

    four-card-feature-section polished

--- four-card-feature-section/README.md
+++ four-card-feature-section/README.md
@@ -1,1 +1,22 @@
-# NextJS (tailwind + ts) Project
+# Frontend Mentor - 3-column preview card component solution
+
+This is a solution to the [Four card feature section challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK).
+
+
+### Screenshot
+
+![Screenshot of the completed challenge](./design/screenshot.png)
+
+
+### Links
+
+- Solution URL: [Solution at Frontend Mentor](https://www.frontendmentor.io/solutions/four-card-feature-section-tailwind-and-nextjs-L61hyCEEkk)
+- Live Site URL: [Live version on Vercel](https://four-card-feature-section-eta-lemon.vercel.app/)
+
+
+### Built with
+
+- [Next.js](https://nextjs.org/) - JavaScript framework
+- [Tailwind CSS](https://tailwindcss.com/) - CSS framework
+
+
--- four-card-feature-section/src/app/page.tsx
+++ four-card-feature-section/src/app/page.tsx
@@ -8,49 +8,52 @@ import teamIcon from "public/icon-team-builder.svg";
 export default function Home() {
   return (
     <div className="flex items-center justify-center bg-veryLightGray min-h-screen text-veryDarkBlue">
-      <main className="flex flex-col items-center justify-center">
-        <h2 className="font-extralight text-[2.265rem] pt-8">
-          Reliable, efficient delivery
-        </h2>
-        <h1 className="font-semibold text-[2.265rem] -mt-1">
-          Powered by Technology
-        </h1>
-        <p className="font-extralight text-[0.975rem] text-center w-[32rem] py-4">
-          Our Artificial Intelligence powered tools use millions of project data
-          points to ensure that your project is successful
-        </p>
+      <main className="flex flex-col items-center justify-center px-8">
+        <div className="pt-12 flex flex-col justify-center items-center">
+          <h2 className="font-extralight text-2xl md:text-[2.265rem] pt-10 md:pt-0">
+            Reliable, efficient delivery
+          </h2>
+          <h1 className="font-semibold text-2xl md:text-[2.265rem] pt-1 md:pt-5">
+            Powered by Technology
+          </h1>
+          <p className="font-extralight text-[0.975rem] text-center w-auto md:w-[32rem] py-4 md:pt-7 md:pb-6">
+            Our Artificial Intelligence powered tools use millions of project
+            data points to ensure that your project is successful
+          </p>
+        </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:grid-rows-2 pt-12 pb-8 p-5 gap-[1.85rem]">
-          <div className="rounded-lg overflow-hidden shadow-xl bg-white w-[21.875rem] h-[15.625rem] xl:row-span-2 xl:my-auto">
-            <div className="px-8 py-7 border-t-4 border-primaryCyan">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:grid-rows-2 pt-12 pb-8 p-0 md:p-10 gap-6 md:gap-[1.85rem]">
+          <div className="justify-self-center flex flex-col justify-between rounded-lg overflow-hidden shadow-xl bg-white w-[20rem] h-42 lg:w-[21.875rem] lg:h-[15.625rem] xl:row-span-2 xl:my-auto">
+            <div className="p-6 md:px-8 md:py-7 border-t-4 border-primaryCyan">
               <h2 className="font-semibold text-[1.225rem] pb-2">Supervisor</h2>
-              <p className="font-extralight text-sm">
+              <p className="font-extralight text-[0.777rem] leading-6 tracking-wide">
                 Monitors activity to identify project roadblocks
               </p>
             </div>
-            <div className="flex justify-end pb-8 pr-8">
+            <div className="flex justify-end pb-6 pr-6 md:pb-8 md:pr-8">
               <Image src={superIcon} alt="" />
             </div>
           </div>
 
-          {/* <div className="flex flex-col space-y-6"> */}
-          <div className="rounded-lg overflow-hidden shadow-xl bg-white w-[21.875rem] h-[15.625rem]">
-            <div className="p-8 border-t-4 border-primaryRed">
-              <h2 className="font-semibold text-lg pb-2">Team Builder</h2>
-              <p className="font-extralight text-sm">
+          <div className="justify-self-center flex flex-col justify-between rounded-lg overflow-hidden shadow-xl bg-white w-[20rem] h-42 lg:w-[21.875rem] lg:h-[15.625rem] lg:h-[15.625rem]">
+            <div className="px-8 py-7 border-t-4 border-primaryRed">
+              <h2 className="font-semibold text-[1.225rem] pb-2">
+                Team Builder
+              </h2>
+              <p className="font-extralight text-[0.777rem] leading-6 tracking-wide">
                 Scans our talent network to create the optimal team for your
                 project
               </p>
             </div>
-            <div className="flex justify-end pb-8 pr-8">
+            <div className="flex justify-end pb-6 pr-6 md:pb-8 md:pr-8">
               <Image src={teamIcon} alt="" />
             </div>
           </div>
 
-          <div className="rounded-lg overflow-hidden shadow-xl bg-white w-[21.875rem] h-[15.625rem] xl:order-last">
-            <div className="p-8 border-t-4 border-primaryOrange">
-              <h2 className="font-semibol text-lg pb-2d">Karma</h2>
-              <p className="font-extralight text-sm">
+          <div className="justify-self-center flex flex-col justify-between rounded-lg overflow-hidden shadow-xl bg-white w-[20rem] h-42 lg:w-[21.875rem] lg:h-[15.625rem] xl:order-last">
+            <div className="px-8 py-7 border-t-4 border-primaryOrange">
+              <h2 className="font-semibold text-[1.225rem] pb-2">Karma</h2>
+              <p className="font-extralight text-[0.777rem] leading-6 tracking-wide">
                 Regularly evaluates our talent to ensure quality
               </p>
             </div>
@@ -58,12 +61,11 @@ export default function Home() {
               <Image src={karmaIcon} alt="" />
             </div>
           </div>
-          {/* </div> */}
 
-          <div className="rounded-lg overflow-hidden shadow-xl bg-white w-[21.875rem] h-[15.625rem] xl:my-auto xl:col-3 xl:row-span-2">
-            <div className="p-8 border-t-4 border-primaryBlue ">
-              <h2 className="font-semibold text-lg pb-2">Calculator</h2>
-              <p className="font-extralight text-sm">
+          <div className="justify-self-center flex flex-col justify-between rounded-lg overflow-hidden shadow-xl bg-white w-[20rem] h-42 lg:w-[21.875rem] lg:h-[15.625rem] xl:my-auto xl:col-3 xl:row-span-2">
+            <div className="px-8 py-7 border-t-4 border-primaryBlue ">
+              <h2 className="font-semibold text-[1.225rem] pb-2">Calculator</h2>
+              <p className="font-extralight text-[0.777rem] leading-6 tracking-wide">
                 Uses data from past projects to provide better delivery
                 estimates
               </p>
