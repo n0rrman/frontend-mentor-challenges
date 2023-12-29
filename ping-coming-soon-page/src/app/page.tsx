2dfc90d19e464c479f4d3a236c919dcfe757b4ad
n0rrman <email@henriknorrman.com>
2023-12-29 23:31:04 +0700

    ping-coming-soon-page desktop layout finished

--- ping-coming-soon-page/src/app/page.tsx
+++ ping-coming-soon-page/src/app/page.tsx
@@ -7,32 +7,35 @@ import EmailForm from "@/components/emailForm";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between items-center min-h-screen">
-      <header>
-        <div className="relative w-36 h-12">
+    <div className="flex flex-col justify-between items-center min-h-screen py-12">
+      <header className="py-9 mt-0.5">
+        <div className="relative w-[5.375rem] h-[1.625rem] ">
           <Image alt="" src={logo.src} fill />
         </div>
       </header>
-      <main className="flex flex-col justify-around items-center">
+      <main className="flex flex-col justify-between items-center w-full sm:w-[40rem]">
         <div className="text-center">
-          <h1 className="text-5xl">We are launching soon!</h1>
-          <p className="text-2xl">Subscribe and get notified</p>
+          <h1 className="text-2xl sm:text-5xl text-gray font-light pb-5">
+            We are launching{" "}
+            <span className="text-veryDarkBlue font-bold ">soon!</span>
+          </h1>
+          <p className="text-sm sm:text-xl">Subscribe and get notified</p>
         </div>
-        <div>
+        <div className="w-full my-2">
           <EmailForm />
         </div>
 
-        <div className="relative w-full aspect-video">
-          <Image alt="" src={dashboardImg.src} fill />
+        <div className="relative w-full aspect-[64/39] mt-12 mb-[2rem]">
+          <Image className="" alt="" src={dashboardImg.src} fill />
         </div>
       </main>
       <footer>
-        <div className="flex flex-row gap-2 justify-center items-center text-primaryBlue">
-          <FaFacebookF className="box-content hover:text-white hover:bg-primaryBlue hover:border-primaryBlue rounded-full p-1 border border-secondaryBlue transition-all duration-200 ease-out" />
-          <FaTwitter className="box-content hover:text-white hover:bg-primaryBlue hover:border-primaryBlue rounded-full p-1 border border-secondaryBlue transition-all duration-200 ease-out" />
-          <FaInstagram className="box-content hover:text-white hover:bg-primaryBlue hover:border-primaryBlue rounded-full p-1 border border-secondaryBlue transition-all duration-200 ease-out" />
+        <div className="flex flex-row gap-3 p-6 pt-6 justify-center items-center text-primaryBlue text-sm">
+          <FaFacebookF className="box-content hover:text-white hover:bg-primaryBlue hover:border-primaryBlue rounded-full p-2 border border-secondaryBlue transition-all duration-200 ease-out" />
+          <FaTwitter className="box-content hover:text-white hover:bg-primaryBlue hover:border-primaryBlue rounded-full p-2 border border-secondaryBlue transition-all duration-200 ease-out" />
+          <FaInstagram className="box-content hover:text-white hover:bg-primaryBlue hover:border-primaryBlue rounded-full p-2 border border-secondaryBlue transition-all duration-200 ease-out" />
         </div>
-        <div className="text-gray">
+        <div className="text-gray text-xs">
           &copy; Copyright Ping. All rights reserved.
         </div>
       </footer>
--- ping-coming-soon-page/src/components/emailForm.tsx
+++ ping-coming-soon-page/src/components/emailForm.tsx
@@ -20,24 +20,29 @@ export default function EmailForm() {
   };
 
   return (
-    <div className="flex flex-col">
-      <form className="flex flex-row items-center" onSubmit={handleSubmit}>
+    <div className="flex flex-col py-7 w-full">
+      <form
+        className="flex flex-col sm:flex-row justify-center items-center w-full"
+        onSubmit={handleSubmit}
+      >
         <input
           className={`${
-            error ? "border-secondaryRed" : "border-secondaryBlue"
-          } w-full z-30 bg-transparent placeholder:text-secondaruBlue py-[0.94rem] px-8 rounded-[2rem] border`}
+            error ? "border-secondaryRed" : "border-gray"
+          } w-full sm:w-2/3 z-30 bg-transparent placeholder:text-gray py-[1rem] px-8 rounded-[2rem] border`}
           placeholder="Your email address..."
           value={input}
           onChange={(e) => handleChange(e.target.value)}
           type="text"
         />
-        <button className="text-white text-sm py-2 shadow-2xl px-12 rounded-[2rem] bg-primaryBlue hover:bg-secondaryBlue duration-200 ease-out transition-all">
+        <button className="text-white ml-0 sm:ml-4 text-sm sm:text-base p-[1rem] w-full  sm:w-[12.7rem] shadow-2xl whitespace-nowrap shadow-secondaryBlue rounded-[2rem] bg-primaryBlue hover:bg-secondaryBlue duration-200 ease-out transition-all">
           Notify Me
         </button>
       </form>
       {error && (
-        <p className="px-8 py-3 text-sm text-secondaryRed">
-          Please provide a valid email
+        <p className="absolute px-8 text-xs text-secondaryRed">
+          <div className="relative top-16">
+            Please provide a valid email address
+          </div>
         </p>
       )}
     </div>
