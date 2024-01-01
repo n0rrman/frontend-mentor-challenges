86a35da2b220d5a48ee838bb00c148fde135e061
n0rrman <email@henriknorrman.com>
2024-01-01 21:13:16 +0700

    ping-coming-soon-page completed

--- ping-coming-soon-page/src/app/page.tsx
+++ ping-coming-soon-page/src/app/page.tsx
@@ -7,35 +7,37 @@ import EmailForm from "@/components/emailForm";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between items-center min-h-screen py-12">
-      <header className="py-9 mt-0.5">
-        <div className="relative w-[5.375rem] h-[1.625rem] ">
+    <div className="flex flex-col justify-between min-h-screen items-center pt-[5.15rem] pb-9 sm:py-12">
+      <header className="pb-9 sm:pt-9 sm:pb-2 mt-0.5">
+        <div className="relative w-[3.3rem] h-4 sm:w-[5.375rem] sm:h-[1.625rem] ">
           <Image alt="" src={logo.src} fill />
         </div>
       </header>
-      <main className="flex flex-col justify-between items-center w-full sm:w-[40rem]">
+      <main className="flex flex-col justify-start sm:justify-center flex-1 items-center w-full sm:w-[40rem] px-7 sm:px-0">
         <div className="text-center">
-          <h1 className="text-2xl sm:text-5xl text-gray font-light pb-5">
+          <h1 className="text-[1.35rem] sm:text-5xl text-gray font-light pb-3.5 sm:pb-5">
             We are launching{" "}
             <span className="text-veryDarkBlue font-bold ">soon!</span>
           </h1>
-          <p className="text-sm sm:text-xl">Subscribe and get notified</p>
+          <p className="text-[0.725rem] sm:text-xl">
+            Subscribe and get notified
+          </p>
         </div>
-        <div className="w-full my-2">
+        <div className="w-full max-w-[20rem] sm:max-w-full my-0 sm:my-2.5 px-5 sm:px-0">
           <EmailForm />
         </div>
 
-        <div className="relative w-full aspect-[64/39] mt-12 mb-[2rem]">
-          <Image className="" alt="" src={dashboardImg.src} fill />
+        <div className="relative w-full aspect-[64/39] mt-10 sm:mt-12 sm:p-0">
+          <Image className="" alt="" src={dashboardImg.src} fill priority />
         </div>
       </main>
-      <footer>
-        <div className="flex flex-row gap-3 p-6 pt-6 justify-center items-center text-primaryBlue text-sm">
+      <footer className="">
+        <div className="flex flex-row gap-3  p-6 justify-center items-center text-primaryBlue text-[0.9rem] sm:text-sm">
           <FaFacebookF className="box-content hover:text-white hover:bg-primaryBlue hover:border-primaryBlue rounded-full p-2 border border-secondaryBlue transition-all duration-200 ease-out" />
           <FaTwitter className="box-content hover:text-white hover:bg-primaryBlue hover:border-primaryBlue rounded-full p-2 border border-secondaryBlue transition-all duration-200 ease-out" />
           <FaInstagram className="box-content hover:text-white hover:bg-primaryBlue hover:border-primaryBlue rounded-full p-2 border border-secondaryBlue transition-all duration-200 ease-out" />
         </div>
-        <div className="text-gray text-xs">
+        <div className="text-gray text-[0.615rem] text-center sm:text-xs">
           &copy; Copyright Ping. All rights reserved.
         </div>
       </footer>
--- ping-coming-soon-page/src/components/emailForm.tsx
+++ ping-coming-soon-page/src/components/emailForm.tsx
@@ -1,50 +1,68 @@
 "use client";
 
-import Image from "next/image";
-
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, FormEventHandler } from "react";
 
 export default function EmailForm() {
   const [input, setInput] = useState("");
   const [error, setError] = useState(false);
 
-  const handleSubmit = (event: FormEvent<HTMLInputElement>): any => {
-    setError(!error);
+  const validateEmail = (email: string): boolean => {
+    if (email.length <= 0) return false;
+    if (email.indexOf("@") < 0) return false;
+    if (email.indexOf(".") < 0) return false;
+    if (email.lastIndexOf(".") == email.length - 1) return false;
+    if (email.indexOf("@") > email.lastIndexOf(".")) return false;
+
+    return true;
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (
+    event: FormEvent<HTMLFormElement>
+  ) => {
     event.preventDefault();
-    console.log("submitted!");
+
+    const validEmail = validateEmail(input);
+    if (validEmail) {
+      setInput("");
+    }
+
+    setError(!validEmail);
   };
 
   const handleChange = (value: string) => {
     setInput(value);
-    console.log(value);
   };
 
   return (
     <div className="flex flex-col py-7 w-full">
       <form
-        className="flex flex-col sm:flex-row justify-center items-center w-full"
+        className="flex flex-col gap-2.5 sm:gap-0 sm:flex-row justify-center items-center w-full"
         onSubmit={handleSubmit}
       >
         <input
           className={`${
             error ? "border-secondaryRed" : "border-gray"
-          } w-full sm:w-2/3 z-30 bg-transparent placeholder:text-gray py-[1rem] px-8 rounded-[2rem] border`}
+          } w-full sm:w-2/3 z-30 bg-transparent text-xs sm:text-base placeholder:text-gray p-3 sm:py-[0.95rem] px-8 rounded-[2rem] border`}
           placeholder="Your email address..."
           value={input}
           onChange={(e) => handleChange(e.target.value)}
           type="text"
         />
-        <button className="text-white ml-0 sm:ml-4 text-sm sm:text-base p-[1rem] w-full  sm:w-[12.7rem] shadow-2xl whitespace-nowrap shadow-secondaryBlue rounded-[2rem] bg-primaryBlue hover:bg-secondaryBlue duration-200 ease-out transition-all">
+        {error && (
+          <div className="relative sm:absolute px-0 sm:px-8 text-xs text-secondaryRed">
+            <p className="relative sm:right-[11.5rem] sm:top-11">
+              Please provide a valid email address
+            </p>
+          </div>
+        )}
+        <button
+          className={`${
+            error ? "mr-0" : "mr-0"
+          } text-white ml-0 sm:ml-4 text-xs sm:text-base p-3 sm:p-[0.97rem] w-full sm:w-[12.7rem] shadow-lg whitespace-nowrap shadow-secondaryBlue rounded-[2rem] bg-primaryBlue hover:bg-secondaryBlue duration-200 ease-out transition-all`}
+        >
           Notify Me
         </button>
       </form>
-      {error && (
-        <p className="absolute px-8 text-xs text-secondaryRed">
-          <div className="relative top-16">
-            Please provide a valid email address
-          </div>
-        </p>
-      )}
     </div>
   );
 }
