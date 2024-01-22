312bfb4de04705103ed5c633270fc310ba4f0dc5
n0rrman <email@henriknorrman.com>
2024-01-22 23:20:31 +0700

    recipe-page scaffolded

---
+++ recipe-page/src/app/fonts.tsx
@@ -0,0 +1,10 @@
+import { Young_Serif, Outfit } from "next/font/google";
+
+export const young = Young_Serif({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+export const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
--- recipe-page/src/app/page.tsx
+++ recipe-page/src/app/page.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+
+import { young, outfit } from "./fonts";
+import foodImage from "/public/image-omelette.jpeg";
+
 export default function Home() {
   return (
-    <div>
-      <main>
+    <div
+      className={`flex justify-center items-center min-h-screen bg-eggshell ${outfit.className}`}
+    >
+      <main className="flex flex-col gap-6 max-w-[46rem] bg-white p-10 rounded-xl my-0 md:my-[7.5rem]">
         <section>
-          <h1>Simple Omelette Recipe</h1>
+          <div className="relative w-full aspect-[2.1866666667] rounded-xl overflow-hidden">
+            <Image src={foodImage} alt="" fill />
+          </div>
+          <h1 className={`${young.className} py-8 text-4xl`}>
+            Simple Omelette Recipe
+          </h1>
 
           <p>
             An easy and quick dish, perfect for any meal. This classic omelette
@@ -11,21 +23,29 @@ export default function Home() {
             your choice of cheese, vegetables, or meats.
           </p>
         </section>
-        <section>
-          <h3>Preparation time</h3>
+        <section className="px-4 py-2 rounded-lg bg-roseWhite">
+          <h3 className="text-darkRaspberry font-bold">Preparation time</h3>
 
-          <ul>
-            <li>Total: Approximately 10 minutes</li>
+          <ul className="list-disc list-inside">
+            <li>
+              <span className="font-bold">Total</span>: Approximately 10 minutes
+            </li>
 
-            <li>Preparation: 5 minutes</li>
-            <li>Cooking: 5 minutes</li>
+            <li>
+              <span className="font-bold">Preparation</span>: 5 minutes
+            </li>
+            <li>
+              <span className="font-bold">Cooking</span>: 5 minutes
+            </li>
           </ul>
         </section>
 
         <section>
-          <h2>Ingredients</h2>
+          <h2 className={`${young.className} text-nutmeg text-2xl pb-5`}>
+            Ingredients
+          </h2>
 
-          <ul>
+          <ul className="list-disc list-inside">
             <li>2-3 large eggs</li>
             <li>Salt, to taste</li>
             <li>Pepper, to taste</li>
@@ -36,43 +56,56 @@ export default function Home() {
           </ul>
         </section>
 
+        <hr />
+
         <section>
-          <h3>Instructions</h3>
-          <ol>
+          <h3 className={`${young.className} text-nutmeg text-2xl pb-5`}>
+            Instructions
+          </h3>
+          <ol className="pl-6 list-decimal list-outside">
             <li>
-              Beat the eggs: In a bowl, beat the eggs with a pinch of salt and
-              pepper until they are well mixed. You can add a tablespoon of
-              water or milk for a fluffier texture.
+              <span className="font-bold">Beat the eggs</span>: In a bowl, beat
+              the eggs with a pinch of salt and pepper until they are well
+              mixed. You can add a tablespoon of water or milk for a fluffier
+              texture.
             </li>
 
             <li>
-              Heat the pan: Place a non-stick frying pan over medium heat and
-              add butter or oil.
+              <span className="font-bold">Heat the pan</span>: Place a non-stick
+              frying pan over medium heat and add butter or oil.
             </li>
 
             <li>
-              Cook the omelette: Once the butter is melted and bubbling, pour in
-              the eggs. Tilt the pan to ensure the eggs evenly coat the surface.
+              <span className="font-bold">Cook the omelette</span>: Once the
+              butter is melted and bubbling, pour in the eggs. Tilt the pan to
+              ensure the eggs evenly coat the surface.
             </li>
 
             <li>
-              Add fillings (optional): When the eggs begin to set at the edges
-              but are still slightly runny in the middle, sprinkle your chosen
-              fillings over one half of the omelette.
+              <span className="font-bold">Add fillings (optional)</span>: When
+              the eggs begin to set at the edges but are still slightly runny in
+              the middle, sprinkle your chosen fillings over one half of the
+              omelette.
             </li>
             <li>
-              Fold and serve: As the omelette continues to cook, carefully lift
-              one edge and fold it over the fillings. Let it cook for another
-              minute, then slide it onto a plate.
+              <span className="font-bold">old and serve</span>: As the omelette
+              continues to cook, carefully lift one edge and fold it over the
+              fillings. Let it cook for another minute, then slide it onto a
+              plate.
             </li>
             <li>
-              Enjoy: Serve hot, with additional salt and pepper if needed.
+              <span className="font-bold">Enjoy</span>: Serve hot, with
+              additional salt and pepper if needed.
             </li>
           </ol>
         </section>
 
+        <hr />
+
         <section>
-          <h3>Nutrition</h3>
+          <h3 className={`${young.className} text-nutmeg text-2xl pb-5`}>
+            Nutrition
+          </h3>
 
           <p>
             The table below shows nutritional values per serving without the
@@ -83,22 +116,22 @@ export default function Home() {
             <tbody>
               <tr>
                 <td>Calories</td>
-                <td>277kcal</td>
+                <td className="text-nutmeg font-bold">277kcal</td>
               </tr>
 
               <tr>
                 <td>Carbs</td>
-                <td>0g</td>
+                <td className="text-nutmeg font-bold">0g</td>
               </tr>
 
               <tr>
                 <td>Protein</td>
-                <td>20g</td>
+                <td className="text-nutmeg font-bold">20g</td>
               </tr>
 
               <tr>
                 <td>Fat</td>
-                <td>22g</td>
+                <td className="text-nutmeg font-bold">22g</td>
               </tr>
             </tbody>
           </table>
