c6e75e2d90beb6f5e66825051c68ecad05955996
n0rrman <email@henriknorrman.com>
2024-02-08 23:51:10 +0700

    base-apparel-coming-soon desktop layout finished

--- base-apparel-coming-soon/src/app/page.tsx
+++ base-apparel-coming-soon/src/app/page.tsx
@@ -9,30 +9,54 @@ import EmailForm from "@/components/emailForm";
 
 export default function Home() {
   return (
-    <main>
-      <div className="relative">
-        <Image alt="" src={logo.src} fill />
-      </div>
-      <div className="relative">
-        <Image className=" " fill alt="" src={bgImg.src} />
-      </div>
-      <div>We&apos;re</div>
-      <div>coming soon</div>
+    <main className="grid grid-cols-1 sm:grid-cols-[0.5763888889fr_0.4236111111fr] min-h-screen">
+      <section id="logo" className="w-full z-30 pt-16 mx-40">
+        <div className="relative w-[9.875rem] h-[2.0625rem]">
+          <Image alt="" src={logo.src} fill />
+        </div>
+      </section>
 
-      <p>
-        Hello fellow shoppers! We&apos;re currently building our new fashion
-        store. Add your email below to stay up-to-date with announcements and
-        our launch deals.
-      </p>
-      <EmailForm />
-      <picture className="">
-        <source
-          className=""
-          media="(min-width: 640px)"
-          srcSet={heroDesktop.src}
-        />
-        <img className="" src={heroMobile.src} alt="" />
-      </picture>
+      <section id="hero" className="row-span-2 z-20 h-full w-full">
+        <picture className="h-full w-full object-cover">
+          <source
+            className="w-full object-cover"
+            media="(min-width: 40rem)"
+            srcSet={heroDesktop.src}
+          />
+          <img
+            className="object-size h-full w-full"
+            src={heroMobile.src}
+            alt=""
+          />
+        </picture>
+      </section>
+
+      <section
+        id="bg"
+        className="absolute grid grid-cols-1 sm:grid-cols-[0.5763888889fr_0.4236111111fr] grid-rows-1 inset-0 z-10"
+      >
+        <div className="relative bg-gradient-to-br from-gradOneStart to-gradOneEnd">
+          <Image className="hidden sm:block" fill alt="" src={bgImg.src} />
+        </div>
+      </section>
+
+      <section id="form" className="flex flex-col text-desaturatedRed">
+        <div className="z-20 mx-40 flex flex-col gap-8">
+          <h1 className="text-7xl uppercase font-light">
+            We&apos;re
+            <span className="block text-darkishRed font-normal">
+              coming soon
+            </span>
+          </h1>
+
+          <p>
+            Hello fellow shoppers! We&apos;re currently building our new fashion
+            store. Add your email below to stay up-to-date with announcements
+            and our launch deals.
+          </p>
+          <EmailForm />
+        </div>
+      </section>
     </main>
   );
 }
