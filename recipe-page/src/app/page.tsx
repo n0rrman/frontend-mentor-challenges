3cb9aafc000f3a755f891224cac6fd5bcd89e844
n0rrman <email@henriknorrman.com>
2024-02-11 11:28:06 +0700

    recipe-page completed

--- recipe-page/src/app/page.tsx
+++ recipe-page/src/app/page.tsx
@@ -8,22 +8,24 @@ export default function Home() {
     <div
       className={`flex justify-center items-center min-h-screen ${outfit.className}`}
     >
-      <main className="flex flex-col gap-7 max-w-[46rem] bg-white md:p-10 rounded-none md:rounded-3xl my-0 md:my-[7.5rem]">
-        <section>
+      <main className="flex flex-col gap-6 max-w-full md:max-w-[46rem] bg-white md:px-10 md:pt-10 md:pb-7 rounded-none md:rounded-3xl my-0 md:my-[7.7rem]">
+        <section className="">
           <div className="relative w-full aspect-[2.1866666667] md:rounded-xl overflow-hidden">
             <Image src={foodImage} alt="" fill />
           </div>
-          <h1 className={`${young.className} pt-8 pb-3.5 text-[2.5rem]`}>
+          <h1
+            className={`${young.className} pt-[1.9rem] pb-3.5 text-3xl md:text-[2.5rem] px-8 md:px-0`}
+          >
             Simple Omelette Recipe
           </h1>
 
-          <p className="text-wengeBrown">
+          <p className="text-wengeBrown px-8 md:px-0">
             An easy and quick dish, perfect for any meal. This classic omelette
             combines beaten eggs cooked to perfection, optionally filled with
             your choice of cheese, vegetables, or meats.
           </p>
         </section>
-        <section className="px-7 py-6 rounded-xl bg-roseWhite space-y-2">
+        <section className="px-7 py-6 mt-2 rounded-xl bg-roseWhite space-y-2 mx-8 md:mx-0">
           <h3 className="text-darkRaspberry font-bold text-xl">
             Preparation time
           </h3>
@@ -35,108 +37,145 @@ export default function Home() {
                 Approximately 10 minutes
               </span>
             </li>
-
-            <li>
-              <span className="font-bold">Preparation</span>: 5 minutes
+            <li className="text-darkRaspberry text-sm">
+              <span className="text-wengeBrown text-base">
+                <span className="ml-[0.8rem] font-bold">Preparation</span>: 5
+                minutes
+              </span>
             </li>
-            <li>
-              <span className="font-bold">Cooking</span>: 5 minutes
+            <li className="text-darkRaspberry text-sm">
+              <span className="text-wengeBrown text-base">
+                <span className="ml-[0.8rem] font-bold">Cooking</span>: 5
+                minutes
+              </span>
             </li>
           </ul>
         </section>
 
-        <section>
-          <h2 className={`${young.className} text-nutmeg text-[1.75rem] pb-5`}>
+        <section className="px-8 md:px-0">
+          <h2
+            className={`${young.className} text-nutmeg text-[1.75rem] pb-4 pt-0.5`}
+          >
             Ingredients
           </h2>
 
-          <ul className="list-disc list-inside flex flex-col gap-4">
-            <li>2-3 large eggs</li>
-            <li>Salt, to taste</li>
-            <li>Pepper, to taste</li>
-            <li>1 tablespoon of butter or oil</li>
+          <ul className="list-disc list-inside flex flex-col gap-2 text-nutmeg text-xs px-2">
             <li>
-              Optional fillings: cheese, diced vegetables, cooked meats, herbs
+              <span className="text-wengeBrown text-base ml-4">
+                2-3 large eggs
+              </span>
+            </li>
+            <li>
+              <span className="text-wengeBrown text-base ml-4">
+                Salt, to taste
+              </span>
+            </li>
+            <li>
+              <span className="text-wengeBrown text-base ml-4">
+                Pepper, to taste
+              </span>
+            </li>
+            <li>
+              <span className="text-wengeBrown text-base ml-4">
+                1 tablespoon of butter or oil
+              </span>
+            </li>
+            <li>
+              <span className="text-wengeBrown text-base ml-4">
+                Optional fillings: cheese, diced vegetables, cooked meats, herbs
+              </span>
             </li>
           </ul>
         </section>
 
-        <hr />
+        <hr className="my-2" />
 
-        <section>
+        <section className="text-sm pt-1 px-8 md:px-0">
           <h3 className={`${young.className} text-nutmeg text-[1.75rem] pb-5`}>
             Instructions
           </h3>
-          <ol className="pl-6 list-decimal list-outside flex flex-col gap-3">
-            <li>
-              <span className="font-bold">Beat the eggs</span>: In a bowl, beat
-              the eggs with a pinch of salt and pepper until they are well
-              mixed. You can add a tablespoon of water or milk for a fluffier
-              texture.
+          <ol className="pl-6 list-decimal list-outside flex flex-col gap-2 py-2 font-bold text-base text-nutmeg">
+            <li className="pl-4">
+              <div className="font-normal text-wengeBrown">
+                <span className="font-bold">Beat the eggs</span>: In a bowl,
+                beat the eggs with a pinch of salt and pepper until they are
+                well mixed. You can add a tablespoon of water or milk for a
+                fluffier texture.
+              </div>
             </li>
 
-            <li>
-              <span className="font-bold">Heat the pan</span>: Place a non-stick
-              frying pan over medium heat and add butter or oil.
+            <li className="pl-4">
+              <div className="font-normal text-wengeBrown">
+                <span className="font-bold">Heat the pan</span>: Place a
+                non-stick frying pan over medium heat and add butter or oil.
+              </div>
             </li>
 
-            <li>
-              <span className="font-bold">Cook the omelette</span>: Once the
-              butter is melted and bubbling, pour in the eggs. Tilt the pan to
-              ensure the eggs evenly coat the surface.
+            <li className="pl-4">
+              <div className="font-normal text-wengeBrown">
+                <span className="font-bold">Cook the omelette</span>: Once the
+                butter is melted and bubbling, pour in the eggs. Tilt the pan to
+                ensure the eggs evenly coat the surface.
+              </div>
             </li>
 
-            <li>
-              <span className="font-bold">Add fillings (optional)</span>: When
-              the eggs begin to set at the edges but are still slightly runny in
-              the middle, sprinkle your chosen fillings over one half of the
-              omelette.
+            <li className="pl-4">
+              <div className="font-normal text-wengeBrown">
+                <span className="font-bold">Add fillings (optional)</span>: When
+                the eggs begin to set at the edges but are still slightly runny
+                in the middle, sprinkle your chosen fillings over one half of
+                the omelette.
+              </div>
             </li>
-            <li>
-              <span className="font-bold">old and serve</span>: As the omelette
-              continues to cook, carefully lift one edge and fold it over the
-              fillings. Let it cook for another minute, then slide it onto a
-              plate.
+            <li className="pl-4">
+              <div className="font-normal text-wengeBrown">
+                <span className="font-bold">Fold and serve</span>: As the
+                omelette continues to cook, carefully lift one edge and fold it
+                over the fillings. Let it cook for another minute, then slide it
+                onto a plate.
+              </div>
             </li>
-            <li>
-              <span className="font-bold">Enjoy</span>: Serve hot, with
-              additional salt and pepper if needed.
+            <li className="pl-4">
+              <div className="font-normal text-wengeBrown">
+                <span className="font-bold">Enjoy</span>: Serve hot, with
+                additional salt and pepper if needed.
+              </div>
             </li>
           </ol>
         </section>
 
         <hr />
 
-        <section>
-          <h3 className={`${young.className} text-nutmeg text-[1.75rem] pb-5`}>
+        <section className="px-8 md:px-0">
+          <h3 className={`${young.className} text-nutmeg text-[1.75rem] pb-1`}>
             Nutrition
           </h3>
 
-          <p>
+          <p className="py-3">
             The table below shows nutritional values per serving without the
             additional fillings.
           </p>
 
           <table className="w-full">
-            <tbody className="flex flex-col">
-              <tr className=" border-b grid grid-cols-2 w-full px-7 py-3">
+            <tbody className="flex flex-col pl-0.5">
+              <tr className=" border-b grid grid-cols-2 w-full px-7 py-[0.67rem]">
                 <td className="">Calories</td>
-                <td className=" text-nutmeg font-bold">277kcal</td>
+                <td className=" text-nutmeg font-bold pl-1.5">277kcal</td>
               </tr>
 
-              <tr className=" border-b grid grid-cols-2 w-full px-7 py-3">
+              <tr className=" border-b grid grid-cols-2 w-full px-7 py-[0.67rem]">
                 <td>Carbs</td>
-                <td className="text-nutmeg font-bold">0g</td>
+                <td className="text-nutmeg font-bold pl-1.5">0g</td>
               </tr>
 
-              <tr className=" border-b grid grid-cols-2 w-full px-7 py-3">
+              <tr className=" border-b grid grid-cols-2 w-full px-7 py-[0.67rem]">
                 <td>Protein</td>
-                <td className="text-nutmeg font-bold">20g</td>
+                <td className="text-nutmeg font-bold pl-1.5">20g</td>
               </tr>
 
-              <tr className=" grid grid-cols-2 w-full px-7 py-3">
+              <tr className=" grid grid-cols-2 w-full px-7 py-[0.67rem]">
                 <td className="">Fat</td>
-                <td className="text-nutmeg font-bold">22g</td>
+                <td className="text-nutmeg font-bold pl-1.5">22g</td>
               </tr>
             </tbody>
           </table>
